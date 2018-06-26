15171e1cd6fd2f4732c85007f10b83a2d1b0db1c
Michael LeGrange <mlegrange15@att.net>
2018-06-26 16:32:23 -0500

    got the questions and answers to dynamically populate the screen on start click with radio buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,82 +1,118 @@
 // On load run the startGame function
 $(document).ready(function() {
 
-    // Function that appends the start button to the screen 
+// Function that appends the start button to the screen 
 startGame = function(){
     $("#start-section").append('<button type="button" class="btn btn-secondary btn-lg btn-block start-button">START</button>');
 }
-    startGame();
-    // launchQuestions();
-
-// Clicking the start button will launch the questions and begin the timer
-$(".start-button").on("click", function() { 
+    
+// Function that will load the questions and begin the timer
+launchGame= function (){
 
     $(".start-button").hide();
 
-    launchQuestions();
- 
- });
+    time = setInterval(decrement, 1000);
 
-// Function that will load the questions and begin the timer
-launchQuestions = function (){
+    for (i=0; i < questionsOptions.length; i++){
 
-    time = setInterval(decrement, 300);
+          var gameDiv = $("<div>");
 
-    for (i=0; i < questions.length; i++){
-        
-        $("#question-section").append(questions[i]);
+          var a = $('<p>' + questionsOptions[i].question + '</p>');
 
-    }  
-   
+          var b = $('<div class="form-check">' + '<input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>' + '<label class="form-check-label" for="exampleRadios1"></label>' + questionsOptions[i].answers[0] + '</div> <div class="form-check">' + '<input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>' + '<label class="form-check-label" for="exampleRadios1"></label>' + questionsOptions[i].answers[1] + '</div> <div class="form-check">' + '<input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>' + '<label class="form-check-label" for="exampleRadios1"></label>' + questionsOptions[i].answers[2] + '</div> <div class="form-check">' + '<input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>' + '<label class="form-check-label" for="exampleRadios1"></label>' + questionsOptions[i].answers[3] + '</div>' );
+
+          gameDiv.append(a,b);
+
+          $("#questions-section").append(gameDiv);
+
+    }
 }
 //  The decrement function.
 function decrement() {
-
     //  Decrease number by one.
     number--;
-
-    //  Show the number in the #show-number tag.
-    $("#question-section").prepend("<h2>" + number + "</h2>");
-
-
+    //  Show the number in the #timer-number tag.
+    $("#timer-section").html("<h2> Time Left: " + number + "</h2>");
     //  Once number hits zero...
     if (number === 0) {
-
-      //  ...run the stop function.
-      stop();
-
+        clearInterval(time);
       //  Alert the user that time is up.
       alert("Time Up!");
     }
   }
 
+startGame();
+
+// Clicking the start button will launch the questions and begin the timer
+$(".start-button").on("click", function() { 
+
+    launchGame();
+ 
+ });
+
+
 });
 
 var time = 0;
 
-var number = 100;
+var number = 30;
 
-var questions = [
-    "Which instrument did Ted steal for Robin in the first episode and appears in Robin’s apartment for the rest of the series?",
-    "What is tattooed on Ted's lower back?",
-    "Barney, Ted, and Marshall all worked for GNB at some point. What does GNB stand for?",
-    "What did Barney have to wear for a year after losing a bet?",
-    "Which of these is one of Barney’s theories?",
-    "Which character was one of Ted’s serious girlfriends?",
-    "Who is Robin’s arch-enemy at work?",
-    "What is the name of Lily’s high school boyfriend, to whom she ~technically~ lost her virginity?",
-    "What is Marshall and Lily’s theory on how to be a happy couple?",
+var questionsOptions = [
+    {
+    question: "Which instrument did Ted steal for Robin in the first episode and appears in Robin’s apartment for the rest of the series?",
+    answers: ["French Horn", "Clarinet", "Tuba", "Drum"],
+    correct: 0,
+    },
+    {
+    question: "What is tattooed on Ted's lower back?",
+    answers: [ "Dolphin", "Rainbow", "Skull","Butterfly"],
+    correct: 3,
+    },
+    {
+    question: "Barney, Ted, and Marshall all worked for GNB at some point. What does GNB stand for?",
+    answers: [ "Gigantic Nation Bank", "George Nation Bank","Goliath Nation Bank", "Good Nation Bank",],
+    correct: 2,
+    },
+    {
+    question: "What did Barney have to wear for a year after losing a bet?",
+    answers: ["Ducky Tie", "Panties", "A Wig", "Necklace"],
+    correct: 0,
+    },
+    {
+    question: "Which of these is one of Barney’s theories?",
+    answers: ["The Mermaid Theory", "The Cloud Theory", "The Player Theory", "The Magician Theory"],
+    correct: 0,
+    },
+    {
+    question: "Which character was one of Ted’s serious girlfriends?",
+    answers: ["Karen", "Stella", "Joni", "Rebecca"],
+    correct: 1,
+    },
+    {
+    question: "Who is Robin’s arch-enemy at work?",
+    answers: ["Patrice", "Steve", "Cindy", "Patrick"],
+    correct: 0,
+    },
+    {
+    question: "What is the name of Lily’s high school boyfriend, to whom she ~technically~ lost her virginity?",
+    answers: ["Shooter", "Scrouge", "Scooter", "Pooter"],
+    correct: 2,
+    },
+    {
+    question: "What is Marshall and Lily’s theory on how to be a happy couple?",
+    answers: ["Olive Theory", "Computer Theory", "Berry Theory", "Cherry Theory"],
+    correct: 0,
+    }
+];
 
-]
-
-var correct = [
-    "French Horn",
-    "Butterfly",
-    "Goliath Nation Bank",
-    "Ducky Tie",
-    "The Mermaid Theory",
-    "Stella",
-    "Patrice",
-    "Scooter",
-    "Olive Theory",
-]
+// var correctAnswers = [
+//     "French Horn",
+//     "Butterfly",
+//     "Goliath Nation Bank",
+//     "Ducky Tie",
+//     "The Mermaid Theory",
+//     "Stella",
+//     "Patrice",
+//     "Scooter",
+//     "Olive Theory",
+// ]
